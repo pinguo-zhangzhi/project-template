d8b8ce052c82859baa301cbd8e03499eb701710e
pinguo-liuhongwei <liuhongwei@camera360.com>
2017-07-19 15:25:21 +0800

    fix ReactDOM is null

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@
 // import 'es6-shim'
 // import 'es6-promise'
 
-import React from 'react'
-import { ReactDOM } from 'react-dom'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import { Router, Route, hashHistory } from 'react-router'
@@ -15,18 +15,18 @@ import configureStore from './store/configure-store'
 // Global styles
 import './styles/index.css'
 // import 'antd/dist/antd.css';
-
+let i = 0
+console.log('i=' + i)
 const store = configureStore({})
 const history = syncHistoryWithStore(createBrowserHistory(), store)
 import defaultRoutes from './routes'
 const routes = defaultRoutes(store)
 console.log('React',React)
 console.log('ReactDOM',ReactDOM)
+console.log('routes',routes)
 ReactDOM.render(
   <Provider store={ store }>
-    <Router history={ history }>
-      { routes }
-    </Router>
+    <Router history={ history } childRoutes={ routes } />
   </Provider>,
   document.getElementById('root')
 )
