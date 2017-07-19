4c1f8e5f5f3bed5f9d431597239e3ff22bef6327
pinguo-liuhongwei <liuhongwei@camera360.com>
2017-07-19 21:12:05 +0800

    fix history version mismatch

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-// import 'es5-shim';
+// import 'es5-shim'
 // import 'es6-shim'
 // import 'es6-promise'
 
@@ -6,27 +6,29 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route } from 'react-router'
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
-import { createBrowserHistory } from 'history'
+import { createHistory } from 'history'
 
 import configureStore from './store/configure-store'
 
 // Global styles
 import './styles/index.css'
-// import 'antd/dist/antd.css';
-let i = 0
-console.log('i=' + i)
-const store = configureStore({})
-const history = syncHistoryWithStore(createBrowserHistory(), store)
-import defaultRoutes from './routes'
-const routes = defaultRoutes(store)
+// import 'antd/dist/antd.css'
+
 console.log('React',React)
 console.log('ReactDOM',ReactDOM)
+console.log('History',History)
+const store = configureStore({})
+const history = syncHistoryWithStore(createHistory(), store)
+import defaultRoutes from './routes'
+const routes = defaultRoutes(store)
 console.log('routes',routes)
 ReactDOM.render(
   <Provider store={ store }>
-    <Router history={ history } childRoutes={ routes } />
+    <Router history={ history } >
+      { routes }
+    </Router>
   </Provider>,
   document.getElementById('root')
 )
